82d0d349f3a68e126e25b0e120f8fd8dadcbb38e
Sean Beard <sean61793@my.uri.edu>
2020-12-30 22:38:42 -0700

    refactor(FeedController): send resp from create_status

--- lib/feed_me_web/controllers/feed_controller.ex
+++ lib/feed_me_web/controllers/feed_controller.ex
@@ -40,25 +40,7 @@ defmodule FeedMeWeb.FeedController do
         IO.puts("No feed item status found for ID #{feed_item_id}")
 
         item = Content.get_feed_item!(feed_item_id, user_id)
-
-        case create_status(conn, item, is_read) do
-          nil ->
-            Conn.send_resp(
-              conn,
-              :internal_server_error,
-              Jason.encode!(%{
-                status: 500,
-                message: "Error updating feed item status"
-              })
-            )
-
-          status ->
-            Conn.send_resp(
-              conn,
-              :ok,
-              Jason.encode!(%{status: 200, isRead: status.is_read})
-            )
-        end
+        create_status(conn, item, is_read)
 
       [status = %AccountContent.FeedItemStatus{}] ->
         IO.puts("Feed item status found for ID #{feed_item_id}")
@@ -106,11 +88,23 @@ defmodule FeedMeWeb.FeedController do
 
     case AccountContent.create_feed_item_status(item, conn.assigns.user, is_read) do
       {:ok, status} ->
-        status
+        Conn.send_resp(
+          conn,
+          :ok,
+          Jason.encode!(%{status: 200, isRead: status.is_read})
+        )
 
       {:error, _changeset} ->
         IO.puts("Error creating feed item status for ID #{item.id}")
-        nil
+
+        Conn.send_resp(
+          conn,
+          :internal_server_error,
+          Jason.encode!(%{
+            status: 500,
+            message: "Error updating feed item status"
+          })
+        )
     end
   end
 end
