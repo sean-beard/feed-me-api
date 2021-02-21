58f524f2a69c04b73d8b95d3c703b81b6438c8ea
Sean Beard <sean61793@my.uri.edu>
2021-02-21 12:38:31 -0700

    config(prod): remove static manifest cache config

--- config/prod.exs
+++ config/prod.exs
@@ -12,8 +12,7 @@ use Mix.Config
 config :feed_me, FeedMeWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "feed-me-digital.gigalixirapp.com/", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
