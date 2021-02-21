59f9170eb0adb35d90eb17c412abe9d6535d18c4
Sean Beard <sean61793@my.uri.edu>
2021-02-21 12:27:33 -0700

    config(prod): update host

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :feed_me, FeedMeWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "feed-me-staging.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "feed-me-digital.gigalixirapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
