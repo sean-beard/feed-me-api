5e1dfd0efa531126d66903a6cb79f33a88ff8294
Sean Beard <sean61793@my.uri.edu>
2021-03-07 22:33:59 -0700

    chore: remove phoenix_live_dashboard dependency

--- lib/feed_me_web/router.ex
+++ lib/feed_me_web/router.ex
@@ -55,12 +55,12 @@ defmodule FeedMeWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  # if Mix.env() in [:dev, :test] do
+  #   import Phoenix.LiveDashboard.Router
 
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: FeedMeWeb.Telemetry
-    end
-  end
+  #   scope "/" do
+  #     pipe_through :browser
+  #     live_dashboard "/dashboard", metrics: FeedMeWeb.Telemetry
+  #   end
+  # end
 end
--- mix.exs
+++ mix.exs
@@ -45,7 +45,6 @@ defmodule FeedMe.MixProject do
       {:jason, "~> 1.0"},
       {:phoenix_ecto, "~> 4.1"},
       {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix, "~> 1.5.3"},
       {:plug_cowboy, "~> 2.0"},
